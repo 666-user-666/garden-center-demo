ecbb6d7eaeb76dca0fb9969521870b933718660c
anna <annetra1997@gmail.com>
2025-06-25 04:36:37 +0300

    Update next.config.mjs for Render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_VERCEL_URL || 'garden-center-demo.onrender.com', // Динамический домен
+        port: '',
+        pathname: '/images/**',
+      },
+      {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000',
